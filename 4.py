762a1a0d043a469eb2986f89d6be11afc932a33b
DelahoutreK <kenandelahoutre@gmail.com>
2024-06-15 19:36:47 +0200

    4 addition pol

--- 4.py
+++ 4.py
@@ -18,7 +18,7 @@ class Polynome: # classe python pour stocker mes polynomes
 #########################################################
 
 # input de polynome 
-    def polcoef1(self):
+    def polcoef(self):
         print("Entrez valeurs de coefs:\n")
         for degree in range(4,0,-1):
             coef = float(input(f"coefs x^{degree}:"))
@@ -27,6 +27,27 @@ class Polynome: # classe python pour stocker mes polynomes
 #########################################################
 
 # addition pol
+    def add(self,oth):
+        resultat = [self.coefficients[i] + oth. coefficientrs[i] for i in range(4)]
+        return Polynome(resultat)
 
+#########################################################
+
+def main():
+    print("Premier Polynome:\n")
+    p1=Polynome()
+    p1.polcoef()
+    print(p1)
+    
+    choix = input("Addition de polynomes?(A)\nMulitiplication par reel?(R)\nMultiplication par polynome?(M)\n")
+    if choix == "A" or "a":
+        print("Second Polynome:\n")
+        p2=Polynome()
+        p2.polcoef()
+        print(p2)
+        sum = p1.add(p2)
+        print(sum)
+    
+    
 #pol = Polynome([2,0,1,3])
 #print(pol)
