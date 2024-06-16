436b476271e833c8b736d538f5dabcc33a9ce964
DelahoutreK <kenandelahoutre@gmail.com>
2024-06-16 17:25:59 +0200

    2 RSA... FIN!

--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
 def keygen(p,q):
     n = p*q #
     phi = (p-1)*(q-1)
+    #print(f"VALEUR DE PHI{phi}\n N {n}\n P {p} \n Q {q}")
     
-
     while True:
         try:
             e = int(input("exposant a utiliser:\n"))        
@@ -12,7 +12,7 @@ def keygen(p,q):
 
             # exposant privé
             d = inv(e, phi)
-        
+            #print(f"VALEUR DE E {e} et D {d}")
             # keys
             return ((e,n),(d,n))
         
@@ -37,14 +37,28 @@ def inv(a,m): # inversé modulaire de a modulo m (entier x tq axx = 1)
     return x1
 
 def encrypt(msg, public_key):
-    e, n = public_key # attribution de valeurs
-    m = int.from_bytes(msg.encode(),"big") # conversion en int
-    c = pow(m, e, n) # chiffrement
-    return c
+    e, n = public_key
+    encryptmsg = [pow(ord(char), e, n) for char in msg] # ord() retourne les codes unicode
+    return encryptmsg
+
+def decrypt(encryptmsg, private_key):
+    d, n = private_key
+    decrypted_chars = [chr(pow(num, d, n)) for num in encryptmsg] # chr() opposé de ord()
+    decrypted_msg = ''.join(decrypted_chars)
+    return decrypted_msg
 
 p = int(input("valeur de p:\n"))
 q = int(input("valeur de q:\n"))
 
+
+
 public_key, private_key = keygen(p,q)
 print("clé pub:\n", public_key)
 print("clé priv:\n",private_key)
+
+message = input('inserez message\n')
+cryptmsg = encrypt(message, public_key)
+print("chiffrement: ", cryptmsg)
+
+decryptmsg = decrypt(cryptmsg, private_key)
+print("message décrypté: ", decryptmsg)
