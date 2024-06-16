ec590b9809f0b0149c89674260481838e4ab3a93
DelahoutreK <kenandelahoutre@gmail.com>
2024-06-16 18:32:25 +0200

    5 custom labels

--- 5.py
+++ 5.py
@@ -5,11 +5,16 @@ set1 = set()
 set2 = set()
 set3 = set()
 
+sets = set1 , set2, set3
+
+numbers = []
+
 for i in range(3):
     while True:
         try:
             nb = int(input(f"chiffre n°{i+1} chiffre de 1 a 10"))
             if 1 <= nb <= 10:
+                numbers.append(nb)
                 break
             else:
                 print("nb de 1 a 10 seulement")
@@ -29,7 +34,8 @@ sort1 = sorted(set1)
 sort2 = sorted(set2)
 sort3 = sorted(set3)
 
-venndeez = venn3([set1,set2,set3],('mult 2', 'mult 3', 'mult 4'))
+venn_labels = tuple(f'multiples de {nb}' for nb in numbers)
+venndeez = venn3(sets, venn_labels)
 
 venndeez.get_label_by_id('100').set_text(','.join(map(str, sorted(set1 - set2 - set3))))
 venndeez.get_label_by_id('010').set_text(','.join(map(str, sorted(set2 - set1 - set3))))
